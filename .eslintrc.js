98a3ea3d527082fbdbe5b2fc32dff82b44416f9a
Raphael Renato <raphaelrenato.medeirosdematta-ext@ab-inbev.com>
2024-05-03 13:06:00 -0300

    chore: add eslint prettier and git commit message linter

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+};
