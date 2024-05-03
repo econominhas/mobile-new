5de20be422a5169fca48164d77a7138134dc5b75
Raphael Renato <raphaelrenato.medeirosdematta-ext@ab-inbev.com>
2024-05-02 22:53:17 -0300

    chore: Create the initial project structure

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-800">
+      <Text className="text-3xl font-semibold text-white">Econominhas</Text>
+    </View>
+  );
+}
