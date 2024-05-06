ed3ac7b6d333538fded811a6055d9ce0fd2c8c28
Raphael Renato <raphaelrenato.medeirosdematta-ext@ab-inbev.com>
2024-05-06 09:49:21 -0300

    chore: add font family

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native';
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-semibold text-gray-600">Econominhas</Text>
+      <Text className="semibold font-subtitle text-4xl text-gray-600">
+        Econominhas
+      </Text>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,32 @@
+import '../styles/global.css';
+
+import { Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import '../styles/global.css';
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="dark" />
